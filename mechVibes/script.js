d7ff8838d5eed2e60bf6158b1bdefdb119e33248
I-amPrashant <airmax50cent@gmail.com>
2024-12-20 12:05:27 +0545

    initialize repo

---
+++ mechVibes/script.js
@@ -0,0 +1,13 @@
+
+// const textFile = document.querySelectorAll('input')[1]; 
+
+// textFile.addEventListener('keydown', function (e) {
+//     const audioFiles=['./sound 5.mp3']
+
+//     console.log(e.key===' ')
+//     const audio = new Audio(`${audioFiles[Math.floor(Math.random() * audioFiles.length)]}`);
+
+//     audio.play();
+   
+// })  
+
